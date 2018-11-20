67b9c38b54f6044a624de3f4ba1ed905f2d2b4d5
altair961 <altair961@users.noreply.github.com>
2018-11-20 11:04:04 +0600

    Added IEventStore interface and EventSourcedAggregate.

---
+++ DroneDelivery/DroneDelivery.Ordering/EventSourcedAggregate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace DroneDelivery.Ordering
+{
+    public abstract class EventSourcedAggregate : Entity
+    {
+        public List<DomainEvent> Changes { get; set; }
+        public int Version { get; set; }
+
+        public EventSourcedAggregate()
+        {
+            Changes = new List<DomainEvent>();
+        }
+
+        public abstract void Apply(DomainEvent changes);
+    }
+}
---
+++ DroneDelivery/DroneDelivery.Ordering/IEventStore.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace DroneDelivery.Ordering
+{
+    public interface IEventStore
+    {
+        void CreateNewStream(string streamName, IEnumerable<DomainEvent> domainEvents);
+        void AppendEventsToStream(string streamName, IEnumerable<DomainEvent> domainEvents, int? expectedVersion);
+        IEnumerable<DomainEvent> GetStream(string streamName, int fromVersion, int toVersion);
+        void AddSnapshot<T>(string streamName, T snapshot);
+        T GetLatestSnapshot<T>(string streamName) where T : class;
+    }
+}
